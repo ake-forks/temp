574aad52cb2583bca8b29c8bc2e3e5119c463f91
Ernesto Garcia <egg@juxt.pro>
2022-09-28 12:32:35 +0200

    Added Workspaces cards for easily trying out UI components.

---
+++ src/darbylaw/workspaces/main.cljs
@@ -0,0 +1,7 @@
+(ns darbylaw.workspaces.main
+  (:require [nubank.workspaces.core :as ws]
+            ; cards namespaces:
+            [darbylaw.workspaces.demo-cards]
+            [darbylaw.workspaces.demo2-cards]))
+
+(defonce init (ws/mount))
