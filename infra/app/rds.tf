85aaa6a377337620d5972a722a3fff8b3e580577
Oliver Marshall <olivershawmarshall@gmail.com>
2022-10-10 16:04:43 +0100

    Remove parameter group

--- infra/app/rds.tf
+++ infra/app/rds.tf
@@ -38,8 +38,6 @@ resource "aws_db_instance" "xtdb-backend" {
   deletion_protection             = true
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 
-  parameter_group_name = "default.mysql5.7"
-
   # TODO: IAM Database Auth?
   # TODO: VPC Security Groups?
   # TODO: Setup mainenance_window and backups
