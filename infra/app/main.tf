12c7ff0e33df0fd1678d3ed05d7a04dccdd2c4dc
Oliver Marshall <Akeboshiwind@users.noreply.github.com>
2022-10-11 13:19:09 +0200

    Setup SSL Termination (#56)
    * Add https listener w/ ssl termination
    
    * Move route53 stuff to route53 file
    
    * Clean up cert validation record
    
    * Move the route53 config back into the app 🤦

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -9,6 +9,7 @@ locals {
   config = lookup(local.environments, terraform.workspace)
 }
 
+
 # >> ECR
 
 resource "aws_ecr_repository" "probatetree" {
@@ -198,85 +199,39 @@ resource "aws_lb_target_group" "probatetree" {
   }
 }
 
+# Redirect all HTTP traffic to use HTTPS
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.ingress.id
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
+    type = "redirect"
+
+    redirect {
+      port        = 443
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# Forward HTTPS traffic onto the container
+# Does TSL termination
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.ingress.id
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy      = "ELBSecurityPolicy-2016-08"
+  certificate_arn = aws_acm_certificate.probatetree.arn
+
+  default_action {
+    type = "forward"
+
     target_group_arn = aws_lb_target_group.probatetree.id
   }
 }
-
-resource "aws_acm_certificate" "probatetree" {
-  domain_name       = "${local.config.subdomain}.${local.config.hosted_zone_name}"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "probatetree" {
-  zone_id = local.config.hosted_zone_id
-  name    = "${local.config.subdomain}.${local.config.hosted_zone_name}"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.ingress.dns_name
-    zone_id                = aws_lb.ingress.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "probatetree_cert_validation" {
-  allow_overwrite = true
-  name            = tolist(aws_acm_certificate.probatetree.domain_validation_options)[0].resource_record_name
-  records         = [tolist(aws_acm_certificate.probatetree.domain_validation_options)[0].resource_record_value]
-  type            = tolist(aws_acm_certificate.probatetree.domain_validation_options)[0].resource_record_type
-  zone_id         = local.config.hosted_zone_id
-  ttl             = 60
-}
-
-resource "aws_acm_certificate_validation" "probatetree" {
-  certificate_arn         = aws_acm_certificate.probatetree.arn
-  validation_record_fqdns = [aws_route53_record.probatetree_cert_validation.fqdn]
-}
-
-# # Redirect all HTTP traffic to use HTTPS
-# resource "aws_lb_listener" "http" {
-#   load_balancer_arn = aws_lb.ingress.id
-#   port              = 80
-#   protocol          = "HTTP"
-#
-#   default_action {
-#     type = "redirect"
-#
-#     redirect {
-#       port        = 443
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
-#
-# # Forward HTTPS traffic onto the container
-# # Does TSL termination
-# resource "aws_lb_listener" "https" {
-#   load_balancer_arn = aws_lb.ingress.id
-#   port              = 443
-#   protocol          = "HTTPS"
-#
-#   ssl_policy      = "ELBSecurityPolicy-2016-08" # NOTE: Is this right?
-#   certificate_arn = ""                          # TODO: This
-#
-#   default_action {
-#     type = "forward"
-#
-#     target_group_arn = aws_lb_target_group.probatetree.id
-#   }
-# }
 
 
 
---
+++ infra/app/route53.tf
@@ -0,0 +1,52 @@
+# >> Route 53
+
+
+# >> Certificates
+
+resource "aws_acm_certificate" "probatetree" {
+  domain_name       = "${local.config.subdomain}.${local.config.hosted_zone_name}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "probatetree" {
+  certificate_arn         = aws_acm_certificate.probatetree.arn
+  validation_record_fqdns = [aws_route53_record.probatetree_cert_validation.fqdn]
+}
+
+
+
+# >> Records
+
+resource "aws_route53_record" "probatetree" {
+  zone_id = local.config.hosted_zone_id
+  name    = "${local.config.subdomain}.${local.config.hosted_zone_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.ingress.dns_name
+    zone_id                = aws_lb.ingress.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "probatetree_cert_validation" {
+  # See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate#referencing-domain_validation_options-with-for_each-based-resources
+  for_each = {
+    for dvo in aws_acm_certificate.probatetree.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+  zone_id         = local.config.hosted_zone_id
+  ttl             = 60
+}
