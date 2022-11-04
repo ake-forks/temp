977f706c626e8f050c7fe0283f5692348ff8b7e2
Ernesto Garcia <egg@juxt.pro>
2022-11-04 18:15:13 +0100

    Moved subscriptions from event code to view code. Solved duplicated key.

--- src/darbylaw/web/ui/settings.cljs
+++ src/darbylaw/web/ui/settings.cljs
@@ -9,44 +9,42 @@
             [darbylaw.web.ui.case-model :as case-model]))
 
 (defn panel [panel-k]
-  [:<>
-   [c/navbar]
-   [mui/toolbar]
-   [mui/drawer {:variant :permanent
-                :anchor :left}
-    [mui/toolbar]
-    [mui/list
-     [mui/list-item {:key :back-to-case}
-      [mui/list-item-button {:onClick #(rf/dispatch
-                                         [::ui/navigate
-                                          [:dashboard
-                                           {:case-id @(rf/subscribe [::case-model/case-id])}]])}
-       [mui/list-item-icon [ui/icon-arrow-back-sharp]]
-       [mui/list-item-text {:primary "Back to case dashboard"}]]]
-     [mui/list-subheader
-      "Your case"]
-     [mui/list-item {:key :user-details}
-      [mui/list-item-button {:selected (= panel-k :user-details-panel)
-                             :onClick #(rf/dispatch
-                                         [::ui/navigate
-                                          [:user-details
-                                           {:case-id @(rf/subscribe [::case-model/case-id])}]])}
-       [mui/list-item-text {:primary "Your details"}]]]
-     [mui/list-item {:key :user-details}
-      [mui/list-item-button {:selected (= panel-k :deceased-details-panel)
-                             :onClick #(rf/dispatch
-                                         [::ui/navigate
-                                          [:deceased-details
-                                           {:case-id @(rf/subscribe [::case-model/case-id])}]])}
-       [mui/list-item-text
-        {:primary (if-let [rel @(rf/subscribe [::case-model/relationship])]
-                    (str "Your " rel "'s details")
-                    "Deceased details")}]]]]]
-   (case panel-k
-     :user-details-panel [user-details/user-details-panel]
-     :deceased-details-panel [deceased-details/panel])
-   [mui/toolbar]
-   [c/footer]])
+  (let [case-id @(rf/subscribe [::case-model/case-id])]
+    [:<>
+     [c/navbar]
+     [mui/toolbar]
+     [mui/drawer {:variant :permanent
+                  :anchor :left}
+      [mui/toolbar]
+      [mui/list
+       [mui/list-item {:key :back-to-case}
+        [mui/list-item-button {:onClick #(rf/dispatch
+                                           [::ui/navigate
+                                            [:dashboard {:case-id case-id}]])}
+         [mui/list-item-icon [ui/icon-arrow-back-sharp]]
+         [mui/list-item-text {:primary "Back to case dashboard"}]]]
+       [mui/list-subheader
+        "Your case"]
+       [mui/list-item {:key :user-details}
+        [mui/list-item-button {:selected (= panel-k :user-details-panel)
+                               :onClick #(rf/dispatch
+                                           [::ui/navigate
+                                            [:user-details {:case-id case-id}]])}
+         [mui/list-item-text {:primary "Your details"}]]]
+       [mui/list-item {:key :deceased-details}
+        [mui/list-item-button {:selected (= panel-k :deceased-details-panel)
+                               :onClick #(rf/dispatch
+                                           [::ui/navigate
+                                            [:deceased-details {:case-id case-id}]])}
+         [mui/list-item-text
+          {:primary (if-let [rel @(rf/subscribe [::case-model/relationship])]
+                      (str "Your " rel "'s details")
+                      "Deceased details")}]]]]]
+     (case panel-k
+       :user-details-panel [user-details/user-details-panel]
+       :deceased-details-panel [deceased-details/panel])
+     [mui/toolbar]
+     [c/footer]]))
 
 (defmethod routes/panels :user-details-panel [panel-k]
   [panel panel-k])
