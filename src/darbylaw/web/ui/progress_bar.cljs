1295d46e132fe15119c146642a3924314a4d6bf9
franc17 <fjb@juxt.pro>
2022-11-10 11:25:08 +0000

    Added tooltip on hover (static). Updated logic to determine which colour assets dot to display

--- src/darbylaw/web/ui/progress_bar.cljs
+++ src/darbylaw/web/ui/progress_bar.cljs
@@ -33,7 +33,7 @@
 
 (defn progress-bar []
   (let [current-case @(rf/subscribe [::current-case])
-        assets (if (some? (:assets current-case)) 2 1)
+        assets (if (some? (:bank-accounts current-case)) 2 1)
         notify (if (= assets 1) 0 1)
         valuations (case notify 0 0 1 0 2 2 3 2 4 2)
         grant (if (= 4 assets notify valuations) 2 0)]
@@ -42,16 +42,21 @@
                    :style {:margin-top "2rem" :margin-bottom "2rem"}}
       [mui/step {:completed true}
        [mui/step-label {:icon (get-icon 4)} "Case Created"]]
-      [mui/step {:completed true}
-       [mui/step-label {:icon (get-icon 3)} "Identity Check"]]
-      [mui/step {:completed (if (> assets 0) true false)}
-       [mui/step-label {:icon (get-icon assets)} "Complete Assets"]]
-      [mui/step {:completed (if (> notify 0) true false)}
-       [mui/step-label {:icon (get-icon notify)} "Notify Institutions"]]
-      [mui/step {:completed (if (> valuations 0) true false)}
-       [mui/step-label {:icon (get-icon valuations)} "Receive Valuations"]]
-      [mui/step {:completed (if (> grant 0) true false)}
-       [mui/step-label {:icon (get-icon grant)} "Grant of Probate"]]]]))
+      [mui/tooltip {:title "We are waiting for SmartSearch to complete your ID check." :position "top"}
+       [mui/step {:completed true}
+        [mui/step-label {:icon (get-icon 3)} "Identity Check"]]]
+      [mui/tooltip {:title "Add bank and utility assets via the dashboard." :position "top"}
+       [mui/step {:completed (if (> assets 0) true false)}
+        [mui/step-label {:icon (get-icon assets)} "Complete Assets"]]]
+      [mui/tooltip {:title "Not available until one or more assets are complete." :position "top"}
+       [mui/step {:completed (if (> notify 0) true false)}
+        [mui/step-label {:icon (get-icon notify)} "Notify Institutions"]]]
+      [mui/tooltip {:title "Not availabe until one or more institutions have been notified." :position "top"}
+       [mui/step {:completed (if (> valuations 0) true false)}
+        [mui/step-label {:icon (get-icon valuations)} "Receive Valuations"]]]
+      [mui/tooltip {:title "Not availabe until all valuations have been received." :position "top"}
+       [mui/step {:completed (if (> grant 0) true false)}
+        [mui/step-label {:icon (get-icon grant)} "Grant of Probate"]]]]]))
 
 
 
