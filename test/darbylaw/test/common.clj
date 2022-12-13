d5ce95cbcadbc52076bd31883a0b5a11ecb2661d
Ernesto Garcia <egg@juxt.pro>
2022-12-13 16:54:24 +0100

    Easier, more composable, test set up.

--- test/darbylaw/test/common.clj
+++ test/darbylaw/test/common.clj
@@ -1,37 +1,36 @@
 (ns darbylaw.test.common
   (:require [clojure.test :refer :all]
             [mount.core :as mount]
-            [darbylaw.config :refer [profile]]
+            [darbylaw.config :refer [profile config]]
             [darbylaw.xtdb-node :refer [xtdb-node]]
+            [darbylaw.handler :refer [ring-handler]]
             [darbylaw.core :refer [web-server]]
-            [xtdb.api :as xt]
-            [darbylaw.config :as config]))
+            [xtdb.api :as xt]))
 
 (def test-states
   {#'profile {:start (fn [] :test)}
    #'xtdb-node {:start (fn [] (xt/start-node {}))
                 :stop (fn [] (.close xtdb-node))}})
 
-(defn use-web-server [f]
-  (mount/stop)
-  (mount/start-with-states test-states)
-  (f)
-  (mount/stop)
-  (mount/start))
-
-(defn use-ring-handler [f]
-  (let [states (mount/except [#'web-server])]
-    (mount/stop states)
-    (-> states
-      (mount/swap-states test-states)
-      (mount/start))
+(defn use-mount-states [states]
+  (fn [f]
+    (mount/stop (keys test-states))
+    (mount/start (-> states
+                   (mount/swap-states test-states)))
     (f)
     (mount/stop states)
     (mount/start states)))
 
+(def ring-handler-states
+  [#'profile #'config #'xtdb-node #'ring-handler])
+
+(def web-server-states
+  (into ring-handler-states
+    [#'web-server]))
+
 (defn make-url [path]
   (str "http://localhost:"
-    (-> config/config :web-server :port)
+    (-> config :web-server :port)
     path))
 
 (defn submap? [m1 m2]
