638a7427291a12e7713f51c9044ef54b75677977
Ernesto Garcia <egg@juxt.pro>
2022-12-14 12:36:23 +0100

    Replacing `put-event` for avoiding long list of parameters.

--- src/darbylaw/api/bank.clj
+++ src/darbylaw/api/bank.clj
@@ -3,29 +3,25 @@
             [reitit.coercion.malli]
             [darbylaw.api.util.xtdb :as xt-util]
             [darbylaw.api.util.http :as http]
-            [darbylaw.api.util.tx-fns :as tx-fns]))
+            [darbylaw.api.util.tx-fns :as tx-fns]
+            [darbylaw.api.case-history :as case-history]))
 
-(defn add-bank-accounts [{:keys [xtdb-node user path-params body-params]}]
+(defn update-bank-accounts [op {:keys [xtdb-node user path-params body-params]}]
   (let [bank-id (:bank-id body-params)
         accounts (:accounts body-params)
         case-id (parse-uuid (:case-id path-params))]
     (when (seq accounts)
-      ; TODO put-event
       (xt-util/exec-tx xtdb-node
         (concat
-          (tx-fns/into* case-id [:bank-accounts :by-bank bank-id] accounts)
-          (tx-fns/conj-unique case-id [:bank-accounts :bank-order] bank-id))))
-    {:status http/status-204-no-content}))
-
-(defn update-bank-accounts [{:keys [xtdb-node user path-params body-params]}]
-  (let [bank-id (:bank-id body-params)
-        accounts (:accounts body-params)
-        case-id (parse-uuid (:case-id path-params))]
-    ; TODO put-event
-    (xt-util/exec-tx xtdb-node
-      (concat
-        (tx-fns/assoc* case-id [:bank-accounts :by-bank bank-id] accounts)
-        (tx-fns/conj-unique case-id [:bank-accounts :bank-order] bank-id)))
+          (case op
+            :add (tx-fns/into* case-id [:bank-accounts :by-bank bank-id] accounts)
+            :update (tx-fns/assoc* case-id [:bank-accounts :by-bank bank-id] accounts))
+          (tx-fns/conj-unique case-id [:bank-accounts :bank-order] bank-id)
+          (case-history/put-event {:event :updated.bank-accounts
+                                   :case-id case-id
+                                   :user user
+                                   :op op
+                                   :accounts accounts}))))
     {:status http/status-204-no-content}))
 
 (def body-schema
@@ -45,11 +41,11 @@
   ["/bank"
    ["/:case-id"
     ["/add-bank-accounts"
-     {:post {:handler add-bank-accounts
+     {:post {:handler (partial update-bank-accounts :add)
              :coercion reitit.coercion.malli/coercion
              :parameters {:body body-schema}}}]
 
     ["/update-bank-accounts"
-     {:post {:handler update-bank-accounts
+     {:post {:handler (partial update-bank-accounts :update)
              :coercion reitit.coercion.malli/coercion
              :parameters {:body body-schema}}}]]])
---
+++ src/darbylaw/api/case_history.clj
@@ -0,0 +1,31 @@
+(ns darbylaw.api.case-history
+  (:require [darbylaw.api.util.tx-fns :as tx-fns]
+            [xtdb.api :as xt]))
+
+(defn put-with-tx-data [m]
+  (tx-fns/invoke ::put-with-tx-data [m]
+    '(fn [ctx m]
+       (let [tx (xtdb.api/indexing-tx ctx)]
+         [[::xt/put (assoc m
+                      :timestamp (::xt/tx-time tx)
+                      :tx-id (::xt/tx-id tx))]]))))
+
+(defn put-event [{:keys [event case-id user] :as event-data}]
+  (assert (keyword? event))
+  (assert (uuid? case-id))
+  (assert (string? (:username user)))
+  (put-with-tx-data
+    (merge
+      (dissoc event-data [:case-id :user])
+      {:xt/id (random-uuid)
+       :type :event
+       :subject-type :probate.case
+       :ref/probate.case.id case-id
+       :event event
+       :by (:username user)})))
+
+(comment
+  (put-event {:event :testing-event
+              :case-id (random-uuid)
+              :user {:username "me"}}))
+
