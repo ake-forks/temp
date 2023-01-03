ea16859444c8f6232678a4c5aef46108c31d9e13
Oliver Marshall <Akeboshiwind@users.noreply.github.com>
2023-01-03 12:39:36 -0800

    Create the doc-store bucket in terraform and use that in the app (#169)

---
+++ infra/app/s3.tf
@@ -0,0 +1,46 @@
+# >> S3
+
+
+# >> Doc Store
+
+resource "aws_s3_bucket" "doc-store" {
+  bucket = "darbylaw-docs-${terraform.workspace}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# TODO: Setup tighter controlled permissions?
+resource "aws_s3_bucket_acl" "doc-store" {
+  bucket = aws_s3_bucket.doc-store.bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "doc-store" {
+  bucket = aws_s3_bucket.doc-store.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "doc-store" {
+  bucket = aws_s3_bucket.doc-store.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
