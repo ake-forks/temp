29d98284c73def58a62d435a4230d825113bb21f
Oliver Marshall <Akeboshiwind@users.noreply.github.com>
2023-01-24 11:37:30 +0000

    Fix scrolling by updating kee-frame (#258)
    * Add updated dependency
    
    * fix: refreshing namespaces broken due to kee-frame's circular dependency

--- dev/user.clj
+++ dev/user.clj
@@ -2,9 +2,18 @@
   (:require
     [mount.core :as mount]
     [clojure.tools.namespace.repl :as ns-tools]
+    [clojure.java.classpath :refer [classpath-directories]]
     [darbylaw.config :refer [profile]]
     [spyscope.core]
     [darbylaw.api.pdf :as pdf]))
+
+;; NOTE: This is a hack to get around the facts that:
+;;       - ns-tools/refresh will refresh all files on the classpath by default
+;;       - Because we've added kee-frame as a git dependency it will be on the classpath
+;;       - kee-frame has a circular dependency in `kee-frame.fsm.beta`
+(apply ns-tools/set-refresh-dirs
+       (->> (classpath-directories)
+            (remove #(re-matches #".*kee-frame.*" (.getAbsolutePath %)))))
 
 (def dev-states
   {#'profile {:start (fn [] :dev)}})
