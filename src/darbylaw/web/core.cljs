ff55c6bdd06d8087916f1e9e225b76a816720b46
Ernesto Garcia <egg@juxt.pro>
2023-02-06 16:52:52 +0100

    Fix: `after-load` was making the whole React tree to re-mount, losing UI state and slowing down the UI development cycle.

--- src/darbylaw/web/core.cljs
+++ src/darbylaw/web/core.cljs
@@ -1,15 +1,19 @@
 (ns darbylaw.web.core
   (:require
-    [re-frame.core :as re-frame]
+    [re-frame.core :as rf]
     [breaking-point.core :as bp]
     [darbylaw.web.views :as views]
     [darbylaw.web.config :as config]
     [day8.re-frame.http-fx]
-    [kee-frame.core :as kf]))
+    [kee-frame.core :as kf]
+    [kee-frame.interop :as kf-interop]))
 
 (defn dev-setup []
   (when config/debug?
     (println "dev mode")))
+
+(def root-component
+  [views/main-panel])
 
 (defn start! []
   (kf/start!
@@ -25,24 +29,24 @@
               ["/app/case/:case-id/deceased-details" :deceased-details]
               ["/app/case/:case-id/view-bank/:bank-id" :view-bank]
               ["/app/case/:case-id/bank-confirmation/:bank-id" :bank-confirmation]]
-     :root-component [views/main-panel]
+     :root-component root-component
      :log (merge {:level :debug}
             (when-not config/debug?
               {:ns-blacklist ["kee-frame.event-logger"]}))}))
 
-(defn ^:dev/after-load mount-root []
-  (re-frame/clear-subscription-cache!)
+(defn init []
+  (rf/dispatch-sync [::bp/set-breakpoints
+                     {:breakpoints [:mobile
+                                    768
+                                    :tablet
+                                    992
+                                    :small-monitor
+                                    1200
+                                    :large-monitor]
+                      :debounce-ms 166}])
+  (dev-setup)
   (start!))
 
-(defn init []
-  (re-frame/dispatch-sync [::bp/set-breakpoints
-                           {:breakpoints [:mobile
-                                          768
-                                          :tablet
-                                          992
-                                          :small-monitor
-                                          1200
-                                          :large-monitor]
-                            :debounce-ms 166}])
-  (dev-setup)
-  (mount-root))
+(defn ^:dev/after-load mount-root []
+  (rf/clear-subscription-cache!)
+  (kf-interop/render-root root-component))
