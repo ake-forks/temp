9dd863b2ce75311a623d318fa69411ec16c2bba1
Oliver Marshall <olivershawmarshall@gmail.com>
2023-03-09 12:53:48 +0000

    fix: improve how icons and tooltips change as part of progress-bar
    Not 100% complete, doesn't deal with asset types other than banks

--- src/darbylaw/web/ui/progress_bar.cljs
+++ src/darbylaw/web/ui/progress_bar.cljs
@@ -3,7 +3,6 @@
     [reagent-mui.components :as mui]
     [reagent.core :as r]
     [re-frame.core :as rf]
-    [darbylaw.web.ui :as ui]
     [darbylaw.web.ui.identity.dialog :as identity-dialog]
     [darbylaw.web.ui.identity.model :as identity-model]))
 
@@ -43,7 +42,8 @@
      label]]])
 
 (defn progress-bar []
-  (let [current-case @(rf/subscribe [::current-case])]
+  (let [current-case @(rf/subscribe [::current-case])
+        institutions (:bank-accounts current-case)]
     [mui/card
      [mui/stepper {:alternative-label true
                    :non-linear true
@@ -59,25 +59,67 @@
                    :pass "Identity checks have passed."
                    "Some manual intervention is required.")
         :icon [identity-dialog/check-icon]}]
-      [stepper-item 
-       {:label "Complete Assets"
-        :tooltip "Add bank and utitity assets via the dashboard."
-        :icon (get-icon
-                (if-not (some? (:bank-accounts current-case))
-                 :available
-                 :tasks-available))}]
-      [stepper-item 
-       {:label "Notify Institutions"
-        :tooltip "We will trigger a notification when one or more assets are complete."
-        :icon (get-icon
-                (if (some? (:bank-accounts current-case))
-                  :available
-                  :unavailable))}]
-      [stepper-item 
-       {:label "Receive Valuations"
-        :tooltip "Not available until one or more institutions have been notified."
-        :icon (get-icon :unavailable)}]
+      (let [status (cond
+                     (and (some? institutions)
+                          (every? :notification-letter institutions))
+                     :completed
+
+                     (some? institutions)
+                     :tasks-available
+
+                     :else :available)]
+        [stepper-item 
+         {:label "Complete Assets"
+          :tooltip (case status
+                     :available "Add bank and utility assets via the dashboard."
+                     :tasks-available "One or more banks left to complete."
+                     :completed "All added banks have been complete!")
+          :icon (get-icon status)}])
+      (let [status (cond
+                     (and (some? institutions)
+                          (every? (comp :sent-at :notification-letter) institutions))
+                     :completed
+
+                     (some :notification-letter institutions)
+                     :tasks-available
+
+                     (some? institutions)
+                     :available
+
+                     :else :unavailable)]
+        [stepper-item 
+         {:label "Notify Institutions"
+          :tooltip (case status
+                     :unavailable "We will trigger a notification when one or more assets are complete."
+                     :available "Waiting on an admin to generate notifications for completed assets."
+                     :tasks-available "Waiting on an admin to send notifications for completed assets."
+                     :completed "All completed assets have had notification letters sent!")
+          :icon (get-icon status)}])
+      (let [status (cond
+                     (and (some? institutions)
+                          (every? :valuation-letter institutions)
+                          (every? (comp (complement empty?) :accounts) institutions)
+                          (->> institutions
+                              (mapcat :accounts)
+                              (every? :confirmed-value)))
+                     :completed
+
+                     (some :valuation-letter institutions)
+                     :tasks-available
+
+                     (some :notification-letter institutions)
+                     :available
+
+                     :else :unavailable)]
+        [stepper-item 
+         {:label "Receive Valuations"
+          :tooltip (case status
+                     :unavailable "Not available until one or more institutions have been notified."
+                     :available "Waiting on receipt of a valuation letter from one or more institutions."
+                     :tasks-available "Complete the valuation of the available assets."
+                     :completed "All assets have been valued!")
+          :icon (get-icon status)}])
       [stepper-item 
        {:label "Grant of Probate"
-        :tooltip "Not available until all valuations have been recieved."
+        :tooltip "Not available until all valuations have been received."
         :icon (get-icon :unavailable)}]]]))
