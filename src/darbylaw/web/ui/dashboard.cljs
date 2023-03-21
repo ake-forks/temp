e9eaba34e304eb0b7c7b4efe68328acb3bc00b14
Oliver Marshall <olivershawmarshall@gmail.com>
2023-03-21 12:01:02 +0000

    refactor(dashboard): move generic dashboard card functions to own namespace

--- src/darbylaw/web/ui/dashboard.cljs
+++ src/darbylaw/web/ui/dashboard.cljs
@@ -5,6 +5,7 @@
     [darbylaw.web.ui.app-layout :as c]
     [darbylaw.web.ui :as ui]
     [darbylaw.web.routes :as routes]
+    [darbylaw.web.util.dashboard :refer [asset-add-button asset-card asset-item]]
     [darbylaw.web.ui.funeral.model :as funeral-model]
     [darbylaw.web.ui.funeral.dialog :as funeral-dialog]
     [darbylaw.web.ui.case-model :as case-model]
@@ -58,32 +59,6 @@
           (str "£"))]]]
      [mui/divider]]))
 
-(defn asset-add-button [{:keys [title on-click]}]
-  [mui/button {:onClick on-click
-               :startIcon (r/as-element [ui/icon-add])
-               :size :large
-               :sx {:width 1
-                    :justify-content :flex-start
-                    :fontSize :body1.fontSize}}
-   (or title "add")])
-
-(defn asset-card-header [title]
-  [:<>
-   [mui/typography {:variant :h5
-                    :sx {:font-weight 600
-                         :mb 1}}
-    title]
-   [mui/divider]])
-
-(defn asset-card [{:keys [title _on-add]} & body]
-  [mui/card {:sx {:border-style :solid
-                  :border-width 1
-                  :border-color :divider}}
-   [mui/card-content {:sx {:paddingTop (ui/theme-spacing 1.5)
-                           "&:last-child" {:paddingBottom (ui/theme-spacing 0.5)}}}
-    [asset-card-header title]
-    (into [:<>] body)]])
-
 (defn bank-card [current-case]
   [asset-card {:title "bank accounts"}
    [bank-dialog/base-dialog]
@@ -113,36 +88,6 @@
            ^{:key k}
            [mui/menu-item {:on-click v} k])
       options)]])
-
-(defn format-currency
-  [value]
-  (format/format "%.2f" value))
-
-(defn asset-item [{:keys [title value on-click icon indent no-divider]}]
-  [mui/box
-   [mui/card-action-area {:on-click on-click
-                          :sx {:padding-top 1 :padding-bottom 1}}
-    [mui/stack (merge
-                 {:direction :row
-                  :spacing 2
-                  :justify-content :space-between}
-                 (when indent
-                   {:sx {:pl (* indent 2)}}))
-     (if (string? icon)
-       [mui/box {:component :img
-                 :src icon
-                 :sx {:width 25 :mr 1}}]
-       icon)
-     [mui/typography {:variant :body1
-                      :noWrap true
-                      :sx {:width "100%"}}
-      title]
-     (when (number? value)
-       [mui/typography {:variant :body1
-                        :sx {:font-weight :bold}}
-        (str "£" (format-currency value))])]]
-   (when-not no-divider
-     [mui/divider])])
 
 ;I think we should move these cards to their respective asset areas (like we have for tasks)
 (defn funeral-card []
