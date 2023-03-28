2124cbb41cc2f6cb7fb9d2f45251d146b442b902
franc17 <fjb@juxt.pro>
2023-03-28 17:23:31 +0100

    removed ordinal from template date. kept ordinal function for possible use later

--- src/darbylaw/api/util/dates.clj
+++ src/darbylaw/api/util/dates.clj
@@ -8,17 +8,20 @@
   (-> (LocalDateTime/ofInstant instant zone-id)
     (.toLocalTime)))
 
+(defn get-ordinal [day-of-month]
+  (if (spec/int-in-range? 10 20 day-of-month)
+    "th"
+    (case (rem day-of-month 10)
+      1 "st"
+      2 "nd"
+      3 "rd"
+      "th")))
+
 (defn long-date [local-date with-day?]
   (let [day-name (str/capitalize
                    (str (.getDayOfWeek local-date) " "))
         day-date (.getDayOfMonth local-date)
-        ordinal (if (spec/int-in-range? 10 20 day-date)
-                  "th"
-                  (case (rem day-date 10)
-                    1 "st"
-                    2 "nd"
-                    3 "rd"
-                    "th"))
+
         month (str/capitalize (str (.getMonth local-date)))
         year (.getYear local-date)]
-    (str (when with-day? day-name) day-date ordinal " " month " " year)))
+    (str (when with-day? day-name) day-date " " month " " year)))
