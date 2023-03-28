b4ae600d2b8b936eeeb837387a80097f4b6ba9f0
franc17 <fjb@juxt.pro>
2023-03-28 18:21:05 +0100

    Changes to verification form

--- src/darbylaw/api/death_cert_verif_template.clj
+++ src/darbylaw/api/death_cert_verif_template.clj
@@ -3,6 +3,7 @@
             [mount.core :as mount]
             [xtdb.api :as xt]
             [darbylaw.api.util.data :as data-util]
+            [darbylaw.api.util.dates :as date-util]
             [darbylaw.api.bank-list :as banks]
             [stencil.api :as stencil]
             [clojure.java.io :as io]))
@@ -18,20 +19,28 @@
     :in '[case-id]}
    case-id])
 
-
-(defn format-date [date]
-  (let [formatter (java.time.format.DateTimeFormatter/ofPattern "dd MMMM yyyy")]
-    (.format date formatter)))
-
 (defn get-letter-template-data [xtdb-node case-id]
-  (let [[case-data]
-        (->> (death-certificate-verification-form-query case-id)
-             (apply xt/q (xt/db xtdb-node))
-             xt-util/fetch-one)
-        today (format-date (java.time.LocalDate/now))]
+  (let [[case-data] (->> (death-certificate-verification-form-query case-id)
+                         (apply xt/q (xt/db xtdb-node))
+                         xt-util/fetch-one)
+        today (date-util/long-date (java.time.LocalDate/now) false)]
     (-> case-data
         (assoc :today today)
+        (assoc-in [:deceased.info :date-of-death]
+          (date-util/long-date
+            (java.time.LocalDate/parse
+              (get-in case-data [:deceased.info :date-of-death])) false))
+        (assoc-in [:deceased.info :date-of-birth]
+          (date-util/long-date
+            (java.time.LocalDate/parse
+              (get-in case-data [:deceased.info :date-of-birth])) false))
         data-util/keys-to-camel-case)))
+
+(comment
+  (assoc-in [:deceased.info :date-of-death]
+    (date-util/long-date (get-in case-data [:deceased-info :date-of-death]) false))
+  (assoc :date-of-death (date-util/long-date (java.time.LocalDate/parse (:date-of-death "case-data")) false)))
+
 
 (mount/defstate death-certificate-verification-form-template
   :start
@@ -49,7 +58,16 @@
 (comment
   (get-letter-template-data
     darbylaw.xtdb-node/xtdb-node
-    (parse-uuid "34a6ff1a-c680-4b51-97f7-f14cebc1fc74"))
+    (parse-uuid "459b205f-bc5e-4c40-a3d0-417480cc5d5f"))
+
+  (def data (get-letter-template-data
+              darbylaw.xtdb-node/xtdb-node
+              (parse-uuid "459b205f-bc5e-4c40-a3d0-417480cc5d5f")))
+  (assoc-in data [:deceased.info :date-of-death]
+    (date-util/long-date
+      (java.time.LocalDate/parse
+        (get-in data [:deceased.info :date-of-death])) false))
+
 
   (defn open-file [file]
     ;; MacOS & linux only
